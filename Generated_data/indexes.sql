e1da9822578bbd02a1eedd13ae766275036f263b
Poutas Sokratis <poutasok@gmail.com>
2021-06-09 23:12:52 +0300

    Add some indexes

---
+++ Generated_data/indexes.sql
@@ -0,0 +1,6 @@
+CREATE INDEX from_index ON visits ( _from);
+CREATE INDEX to_index ON visits ( _to);
+
+CREATE INDEX customer_id_number ON customer ( id_number);
+
+CREATE INDEX charge_time_index ON uses ( charge_time);
